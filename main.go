88461dfef12ae072246f68112d2bb35589312ac2
Daniel Rocha <rochadaniel@acad.ifma.edu.br>
2022-07-03 22:09:30 -0300

    style(main.go): comments removed

--- main.go
+++ main.go
@@ -1,26 +1,9 @@
 package main
 
 import (
-	"github.com/dannrocha/czen/src"
+	application "github.com/dannrocha/czen/src"
 )
 
-// import application "github.com/dannrocha/czen/src"
-
 func main() {
-	// filter := script.ScriptHandle{
-	// 	Script: `
-	// 		function run(argument)
-	// 			return string.lower(argument)
-	// 		end
-	// 	`,
-	// 	Args: []string{
-	// 		"Daniel",
-	// 	},
-	// }
-
-	// value := filter.Run()
-
-	// fmt.Println(value)
-
 	application.Run()
 }
