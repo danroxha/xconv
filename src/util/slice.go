73926e562285951c4ee6384c80397e8f4d28de6d
Daniel Rocha <rochadaniel@acad.ifma.edu.br>
2022-08-02 20:28:28 -0300

    style(util/slice.go): removed blank line

--- src/util/slice.go
+++ src/util/slice.go
@@ -29,7 +29,6 @@ func ContainsSlice[T comparable](slice []T, item T) bool {
 	return false
 }
 
-
 func ReverseSlice[T comparable](s []T) []T {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
     s[i], s[j] = s[j], s[i]
